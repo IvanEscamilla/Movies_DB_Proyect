c589f1698f8c629b430fa2916bb1a9d1d0be69d8
IvanEscamilla <liee0177@gmail.com>
2016-04-16 05:28:45 -0500

    test-post-success

--- app/app.js
+++ app/app.js
@@ -3,9 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.movie_search_view',
+  'movie_search_view',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/search'});
-}]);
+}]).run(function($rootScope)
+{
+  $rootScope.response = [];
+});
