dbdea8a280a25c49439bdcc12f900aa54785472a
IvanEscamilla <liee0177@gmail.com>
2016-04-18 06:27:41 -0500

    services integration and card of movies working

--- app/movie_search_view/movie_search_services.js
+++ app/movie_search_view/movie_search_services.js
@@ -3,10 +3,40 @@
  */
 
 var moviesWebServices = angular.module('moviesWebServices', ['ngResource']);
-
+/*
 moviesWebServices.factory('moviePost', ['$resource',
     function($resource){
-        return $resource('moviePost/:movies.json', {}, {
-            query: {method:'POST', params:{movie_info:'back to the future'}, isArray:true}
+        return $resource('http://localhost:120/admin/movies/get_movie', {}, {
+            query: {
+                method: 'POST',
+                params: {data: JSON.stringify({movie_info: '%Tony%'})},
+                isArray: false,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+            }});
+    }]);
+*/
+moviesWebServices.factory('moviePost', ['$resource',
+    function($resource){
+        return $resource('', {}, {
+            get: {
+                url: 'http://localhost:120/admin/movies/get_movie',
+                method: 'POST',
+                params: {data: '@data'},
+                isArray: false,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+            },
+            add: {
+                url: 'http://localhost:120/admin/insert/new_movie',
+                method: 'POST',
+                params: {data: '@data'},
+                isArray: false,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+            },
+            get_movies_json: {
+                url: 'movie_search_view/movies_json/movies.json',
+                method: 'GET',
+                params: {},
+                isArray: true,
+            },
         });
     }]);
--- app/movie_search_view/movie_search_view.js
+++ app/movie_search_view/movie_search_view.js
@@ -11,17 +11,100 @@
         });
     }]);
 
-    test.controller('movieSearchViewCtrl',function ($scope, $rootScope, $mdSidenav) {
+    test.controller('movieSearchViewCtrl',function ($scope, $rootScope, $mdSidenav, moviePost) {
+        $scope.movieArr = [];
+        moviePost.get({data:JSON.stringify({movie_info: '%back to the future%'})}).$promise.then(function(response) {
+            // success
+            if(response.status == "OK")
+            {
+                $scope.movieArr = response.result;
+            }
+        }, function(errResponse) {
+            // fail
+        });
+
         $scope.openSettingsSideNav = function()
         {
             var settingsSN = $mdSidenav('settingsSideNav');
             settingsSN.open();
         };
 
-        
+        $scope.openSearchTextField = function()
+        {
+            var settingsSN = $mdSidenav('settingsSideNav');
+            settingsSN.open();
+        };
+
+        $scope.searchMovie = function()
+        {
+            console.log($scope.search);
+           moviePost.get({data:JSON.stringify({movie_info: '%'+$scope.search+'%'})}).$promise.then(function(response) {
+                // success
+                if(response.status == "OK")
+                {
+                    $scope.movieArr = response.result;
+                }
+            }, function(errResponse) {
+                // fail
+            });
+        };
+
+        $scope.returnGenders = function(gendersObj)
+        {
+            var genders = "";
+
+            for(var i = 0; i < gendersObj.length; i++)
+            {
+                genders += gendersObj[i].gender + " ";
+            }
+
+            return genders;
+        };
+
+        $scope.returnCountries = function(countriesObj)
+        {
+            var countries = "";
+
+            for(var i = 0; i < countriesObj.length; i++)
+            {
+                countries += countriesObj[i].country + " ";
+            }
+
+            return countries;
+        };
+
+        $scope.returnLanguages = function(languagesObj)
+        {
+            var languages = "";
+
+            for(var i = 0; i < languagesObj.length; i++)
+            {
+                languages += languagesObj[i].language + " ";
+            }
+
+            return languages;
+        };
+
     });
 
-    test.controller('settingsSideNavCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    test.directive('doSearch', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if(event.which === 13) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.doSearch);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+
+    test.controller('settingsSideNavCtrl', function ($scope, $timeout, $mdSidenav, moviePost) {
+
+        var mov = moviePost.get_movies_json();
+
         $scope.close = function () {
             // Component lookup should always be available since we are not using `ng-if`
             $mdSidenav('left').close()
@@ -30,6 +113,15 @@
                 });
         };
 
+        $scope.initMovies = function()
+        {
+            for(var i = 0; i<mov.length;i++)
+            {
+                moviePost.add({data:JSON.stringify({movie: mov[i]})});
+            }
+        };
+
+
         $(document).off('mouseenter');
         $(document).on('mouseenter','md-list-item',function() {
             var svg = document.getElementById('movie_film');
@@ -37,4 +129,23 @@
             element.setAttribute("fill", "rgb(216, 0, 39)");
 
             });
+    });
+
+
+    test.config(function($mdThemingProvider) {
+        //change default color for warn
+        var white = $mdThemingProvider.extendPalette('grey', {
+            '500': 'ffffff'
+        });
+        $mdThemingProvider.definePalette('grey', white);
+        $mdThemingProvider.theme('input', 'default')
+            .primaryPalette('blue-grey').dark().foregroundPalette[3]  ='rgba(198,198,198,0.9)';
+
+        $mdThemingProvider.theme('toolbar', 'default')
+            .primaryPalette('light-blue').dark().foregroundPalette[3]  ='rgba(255,255,255,0.9)';
+
+        // Configure a dark theme with primary foreground yellow
+        $mdThemingProvider.theme('docs-dark', 'default')
+            .primaryPalette('grey')
+            .dark();
     });
